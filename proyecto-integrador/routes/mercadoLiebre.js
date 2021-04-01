0bccc761fe3c473b5e28f006c8543aa0856fec07
mrosino <mrosino@udesa.edu.ar>
2021-04-01 15:26:49 -0300

    rutas ok

--- proyecto-integrador/routes/mercadoLiebre.js
+++ proyecto-integrador/routes/mercadoLiebre.js
@@ -2,4 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 let mlController = require('../controllers/mlController');
-router.get('/', mlController.listadoBandas);
+
+router.get('/', mlController.index);
+
+router.get('/products', mlController.products);
+
+router.get('/register', mlController.register);
+
+router.get('/login', mlController.login);
+
+router.get('/productAdd', mlController.productAdd);
+
+router.get('/profile', mlController.profile);
+
+router.get('/searchResult', mlController.searchResult);
+
+router.get('/profileEdit', mlController.profileEdit);
+
+module.exports = router;
+
