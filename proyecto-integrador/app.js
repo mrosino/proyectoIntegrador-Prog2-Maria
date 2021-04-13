cf8252963d599ff59ac8df987bbe28008ba0d239
Adela <69322992+addbuye@users.noreply.github.com>
2021-04-13 18:54:04 -0300

    correccion app

--- proyecto-integrador/app.js
+++ proyecto-integrador/app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
 var ramoRouter = require('./routes/ramo');
 var userRamoRouter = require('./routes/userRamo');
 var productRouter=require('./routes/product');
@@ -28,7 +28,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 app.use('/ramo', ramoRouter);
 app.use('/ramo', userRamoRouter);
 app.use('/ramo', productRouter);
