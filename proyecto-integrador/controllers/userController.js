fcf059fb4e3727f8505d8bf071f9c3d103198cc6
Adela <69322992+addbuye@users.noreply.github.com>
2021-04-26 15:35:59 -0300

    indexation

--- proyecto-integrador/controllers/userController.js
+++ proyecto-integrador/controllers/userController.js
@@ -1,12 +1,21 @@
 let userController = {
   login: (req, res) => {
-    res.render("login", { title: "Pagina de login", logged: false });
+    res.render("login", {
+      title: "Pagina de login",
+      logged: false,
+    });
   },
   register: (req, res) => {
-    res.render("register", { title: "Pagina de registracion", logged: false });
+    res.render("register", {
+      title: "Pagina de registracion",
+      logged: false,
+    });
   },
   profile: (req, res) => {
-    res.render("profile", { title: "Pagina de perfil", logged: true });
+    res.render("profile", {
+      title: "Pagina de perfil",
+      logged: true,
+    });
   },
   profileEdit: (req, res) => {
     res.render("profileEdit", {
