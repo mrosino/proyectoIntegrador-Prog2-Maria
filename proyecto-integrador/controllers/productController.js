ae351cd54bfc173a9337c022bfbb06a0c039a582
Adela <69322992+addbuye@users.noreply.github.com>
2021-05-24 22:38:32 -0300

    law and order

--- proyecto-integrador/controllers/productController.js
+++ proyecto-integrador/controllers/productController.js
@@ -3,83 +3,80 @@ const { resolveInclude } = require("ejs");
 const Op = db.Sequelize.Op;
 
 let productController = {
-  products: (req, res) => {
-    let id = req.params.id;
-    db.Products.findByPk(id)
-      .then((data) => {
-        db.Comments.findAll({
-          raw: true,
-          where: { product_id: id },
-          order: [["creation_date", "DESC"]],
-        })
-        .then((info) => {
-          return res.render("products", {
-            comments: info,
-            products: data.dataValues, //aca pido el nombre de la base de datos
-            title: "Pagina de detalle de productos",
-          });
+    products: (req, res) => {
+        let id = req.params.id;
+        db.Products.findByPk(id)
+            .then((data) => {
+                db.Comments.findAll({
+                    raw: true,
+                    where: { product_id: id },
+                    order: [
+                        ["creation_date", "DESC"]
+                    ],
+                }).then((info) => {
+                    return res.render("products", {
+                        comments: info,
+                        products: data.dataValues, //aca pido el nombre de la base de datos
+                        title: "Pagina de detalle de productos",
+                    });
+                });
+            })
+            .catch((error) => {
+                return resolve.send(error);
+            });
+    },
+    commentAdd: (req, res) => {
+        db.Comments.create({
+                product_id: req.body.id,
+                creator_id: res.locals.user.id,
+                content: req.body.comment,
+            })
+            .then(() => {
+                return res.redirect(req.headers.referer);
+            })
+            .catch((error) => {
+                throw error;
+            });
+    },
+    commentDelete: (req, res) => {
+        db.Comments.destroy({
+                where: { id: req.body.id },
+            })
+            .then(() => {
+                return res.redirect(req.headers.referer);
+            })
+            .catch((error) => {
+                throw error;
+            });
+    },
+    productAdd: (req, res) => {
+        res.render("productAdd", {
+            title: "Pagina de agregar producto ",
         });
-      })
-      .catch((error) => {
-        return resolve.send(error);
-      });
-  },
-  commentAdd: (req, res) => {
-    db.Comments.create({
-      product_id: req.body.id,
-      creator_id: res.locals.user.id,
-      content: req.body.comment,
-    })
-      .then(() => {
-        return res.redirect(req.headers.referer);
-      })
-      .catch((error) => {
-        throw error;
-      });
-  },
-  commentDelete: (req, res) => {
-    db.Comments.destroy({
-      where: { id: req.body.id },
-    })
-    .then(()=>{
-      return res.redirect(req.headers.referer)
-    })
-    .catch((error) => {
-      throw error;
-    });
-      
-  },
-  productAdd: (req, res) => {
-    res.render("productAdd", {
-      title: "Pagina de agregar producto ",
-    });
-  },
+    },
 
-  productEdit: (req, res) => {
-    res.render("productEdit", {
-      title: "Pagina de agregar producto ",
-      id: req.params.id,
-    });
-  },
-  productEdited: (req, res) => {
-    let editId = req.body.id;
-    db.Products.update(
-      {
-        product_name: req.body.product_name,
-        description: req.body.description,
-        //fatla ver para cambiar la imagen
-      },
-      {
-        where: { id: editId },
-      }
-    )
-      .then(() => {
-        return res.redirect(`/ramo/products/${editId}`);
-      })
+    productEdit: (req, res) => {
+        res.render("productEdit", {
+            title: "Pagina de agregar producto ",
+            id: req.params.id,
+        });
+    },
+    productEdited: (req, res) => {
+        let editId = req.body.id;
+        db.Products.update({
+                product_name: req.body.product_name,
+                description: req.body.description,
+                //fatla ver para cambiar la imagen
+            }, {
+                where: { id: editId },
+            })
+            .then(() => {
+                return res.redirect(`/ramo/products/${editId}`);
+            })
 
-      .catch((error) => {
-        return res.send(error);
-      });
-  },
+        .catch((error) => {
+            return res.send(error);
+        });
+    },
 };
 module.exports = productController;
--- proyecto-integrador/controllers/securityController.js
+++ proyecto-integrador/controllers/securityController.js
@@ -2,83 +2,78 @@ const db = require("../database/models");
 const Op = db.Sequelize.Op;
 const bcrypt = require("bcryptjs");
 var securityController = {
-  login: (req, res) => {
-    return res.render("security/login", {
-      failed: req.query.failed,
-      
-    });
-  },
-  authenticate: (req, res) => {
-    db.Users.findOne({ where: { email: req.body.email } })
+    login: (req, res) => {
+        return res.render("security/login", {
+            failed: req.query.failed,
+        });
+    },
+    authenticate: (req, res) => {
+        db.Users.findOne({ where: { email: req.body.email } })
 
-      .then((user) => {
-        if (user != undefined) {
-          if (bcrypt.compareSync(req.body.password, user.password)) {
-            req.session.user = user;
-            req.session.logged = true;
-            req.session.save();
+        .then((user) => {
+                if (user != undefined) {
+                    if (bcrypt.compareSync(req.body.password, user.password)) {
+                        req.session.user = user;
+                        req.session.logged = true;
+                        req.session.save();
 
-            return res.redirect(`/ramo/profile/${user.id}`);
-          }
-        }
+                        return res.redirect(`/ramo/profile/${user.id}`);
+                    }
+                }
 
-        return res.redirect(req.headers.referer);
-      })
-      .catch((error) => {
-        return res.redirect(req.headers.referer);
-      });
-  },
-  logout: (req, res) => {
-    req.session.destroy();
-    return res.redirect("/");
-  },
-  editedUser: (req, res) => {
-    db.Users.findOne({ where: { email: req.body.email } })
-      .then((user) => {
-        if (!user) {
-          db.Users.update({
-            email: req.body.email,
-          }, {
-            where: { id: req.body.id }
-          })
-            .then(function (data) {
-              console.log(data)
-              res.redirect('/ramo/profile/' + req.body.id);
+                return res.redirect(req.headers.referer);
             })
-            .catch(function (error) {
-              console.log(error)
-            })
-        } else {
-          res.redirect(req.headers.referer)
-        }
-      })
-
-  },
-  editedPass:(req, res) => {
-    
-    db.Users.findOne({ where: { password: req.body.password } })
-      .then((user) => {
-        if (!user) {
-          let encryptedPss= bcrypt.hashSync(req.body.password);
-          db.Users.update({
-            password:encryptedPss,
-          }, {
-            where: { id: req.body.id }
-          })
-            .then(function (data) {
-              console.log(data)
-              res.redirect('/ramo/profile/' + req.body.id);
-            })
-            .catch(function (error) {
-              console.log(error)
-            })
-        } else {
-          res.redirect(req.headers.referer)
-        }
-      })
-
-  },
-
+            .catch((error) => {
+                return res.redirect(req.headers.referer);
+            });
+    },
+    logout: (req, res) => {
+        req.session.destroy();
+        return res.redirect("/ramo");
+    },
+    editedUser: (req, res) => {
+        db.Users.findOne({ where: { email: req.body.email } }).then((user) => {
+            if (!user) {
+                db.Users.update({
+                        email: req.body.email,
+                    }, {
+                        where: { id: req.body.id },
+                    })
+                    .then(function(data) {
+                        console.log(data);
+                        res.redirect("/ramo/profile/" + req.body.id);
+                    })
+                    .catch(function(error) {
+                        console.log(error);
+                    });
+            } else {
+                res.redirect(req.headers.referer);
+            }
+        });
+    },
+    editedPass: (req, res) => {
+        db.Users.findOne({ where: { password: req.body.password } }).then(
+            (user) => {
+                if (!user) {
+                    let encryptedPss = bcrypt.hashSync(req.body.password);
+                    db.Users.update({
+                            password: encryptedPss,
+                        }, {
+                            where: { id: req.body.id },
+                        })
+                        .then(function(data) {
+                            console.log(data);
+                            res.redirect("/ramo/profile/" + req.body.id);
+                        })
+                        .catch(function(error) {
+                            console.log(error);
+                        });
+                } else {
+                    res.redirect(req.headers.referer);
+                }
+            }
+        );
+    },
 };
 
 module.exports = securityController;
