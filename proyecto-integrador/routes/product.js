a46e393546e264755cf0a86c6ad8de941b2c7d97
mrosino <mrosino@udesa.edu.ar>
2021-06-07 09:49:24 -0300

    instalacion de multer

--- proyecto-integrador/routes/product.js
+++ proyecto-integrador/routes/product.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
-let db = require('../database/models')
+let db = require('../database/models');
+const multer = require ('multer');
+const path = require ('path');
 let productController = require('../controllers/productController');
 
 router.get('/products/:id?', productController.products);
@@ -10,7 +12,7 @@ router.post('/products/commentDelete', productController.commentDelete);
 
 router.get('/productAdd', productController.productAdd);
 
-router.post('/productAdd', productController.productAdded);
+router.post('/productAdd', upload.single('imagen') ,productController.productAdded);
 router.post('/products/productDelete', productController.productDelete);
 
 router.get('/productEdit/:id', productController.productEdit);
