cf465c47562512b725ca2be9dc00dae399d8bd33
Adela <69322992+addbuye@users.noreply.github.com>
2021-06-13 23:08:09 -0300

    algo rompi

--- proyecto-integrador/controllers/followersController.js
+++ proyecto-integrador/controllers/followersController.js
@@ -15,44 +15,17 @@ let followersController = {
       followed_by: req.body.by,
       follows: req.body.toFollow,
     });
+    return res.redirect(`/ramo/profile/${req.body.toFollow}`);
 
-    // db.Users.findByPk(req.body.by)
-    //   .then(() => {
-    //     db.Users.update({
-    //       follows: req.body.toFollow,
-    //       userUpdate_date: new Date().getTime(),
-    //     }).then(() => {
-    //       db.Users.findByPk(req.body.toFollow).then(() => {
-    //         db.Users.update({
-    //           follows: req.body.by,
-    //           userUpdate_date: new Date().getTime(),
-    //         }).then(() => {
-    //           return res.redirect(`/ramo/profile/${req.body.toFollow}`);
-    //         });
-    //       });
-    //     });
-      //})
-      // .catch(function (error) {
-      //   throw error;
-      // });
   },
 
   unfollowed: (req, res) => {
-    let iduser = req.params.id;
-
-    db.Follower.findOne({
-      where: {
-        id_follower: iduser,
-      },
-    }).then(function (user) {
-      db.Follower.destroy({
-        where: {
-          id: user.id,
-        },
-      }).then(function () {
-        res.redirect("/profile");
-      });
+    db.Follower.destroy({
+      followed_by: req.body.by,
+      follows: req.body.toFollow,
     });
+    return res.redirect(`/ramo/profile/${req.body.toFollow}`);
+   
   },
 };
 
